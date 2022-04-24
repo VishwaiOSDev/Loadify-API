b1c681e3efe432b639168841d92cb215c738ea71
Vishwa <vishwa300499@icloud.com>
2022-04-24 19:00:48 +0530

    ytdl code sample working

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const fs = require("fs");
 const PORT = 3200;
+
+const ytdl = require("ytdl-core");
 
 // Middleware
 app.use(express.json());
@@ -11,6 +14,29 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://localhost/YouTube_Downloader");
 }
+
+app.get("/view", async (req, res) => {
+  const video_url = req.query.url;
+  const video = ytdl(video_url, { quality: 137 });
+
+  video.on("progress", function (info) {
+    console.log("Downloading...");
+  });
+
+  video.on("end", function (info) {
+    console.log("Downloaded...");
+  });
+
+  video.pipe(fs.createWriteStream("video1.mp4"));
+});
+
+app.get("/download", async (req, res) => {
+  const v_id = req.query.url.split("v=")[1];
+  const info = await ytdl.getInfo(req.query.url);
+  console.log(info);
+  console.log(info.formats[4]);
+  console.log(info.formats[1]);
+});
 
 app.listen(PORT, function () {
   console.log(`Server is running on ${PORT}`);
