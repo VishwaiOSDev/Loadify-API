265c47da6a6048f28e2b833f57c44712e029ceb9
Vishwa <vishwa300499@icloud.com>
2022-05-02 12:25:55 +0530

    Video & Audio Mux is completed

--- routes/download_video.js
+++ routes/download_video.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const ytdl = require("ytdl-core");
 const fs = require("fs");
+const ffmpeg = require("ffmpeg-static");
+const stream = require("stream");
+const cp = require("child_process");
+const readline = require("readline");
 const constants = require("../lib/constants");
 
 // Routers
@@ -22,7 +26,7 @@ router.get("/mp4", async (req, res) => {
   const video_url = req.query.url;
   const video_id = req.query.url.split("v=")[1];
   const video_details = await getVideoDetails(video_id);
-  const video = ytdl(video_url);
+  const video = ytdl(video_url, { quality: 137 });
   video.pipe(
     fs.createWriteStream(`./videos/YouTube/${video_details.title}.mp4`)
   );
@@ -34,12 +38,102 @@ router.get("/mp3", async (req, res) => {
   const video_url = req.query.url;
   const video_id = req.query.url.split("v=")[1];
   const video_details = await getVideoDetails(video_id);
-  const video = ytdl(video_url, { filter: "audioonly" });
+  const video = ytdl(video_url, { quality: "highestaudio" });
   video.pipe(
     fs.createWriteStream(`./audios/YouTube/${video_details.title}.mp3`)
   );
   // Give the downloaded file to the client
   res.status(200).json({ message: "Audio File Downloaded" });
+});
+
+router.get("/mux", async (req, res) => {
+  const ref = req.query.url;
+  const tracker = {
+    start: Date.now(),
+    audio: { downloaded: 0, total: Infinity },
+    video: { downloaded: 0, total: Infinity },
+    merged: { frame: 0, speed: "0x", fps: 0 },
+  };
+
+  // Get audio and video streams
+  const audio = ytdl(ref, { quality: "highestaudio" }).on(
+    "progress",
+    (_, downloaded, total) => {
+      tracker.audio = { downloaded, total };
+    }
+  );
+  const video = ytdl(ref, { quality: "137" }).on(
+    "progress",
+    (_, downloaded, total) => {
+      tracker.video = { downloaded, total };
+    }
+  );
+
+  // Start the ffmpeg child process
+  const ffmpegProcess = cp.spawn(
+    ffmpeg,
+    [
+      // Remove ffmpeg's console spamming
+      "-loglevel",
+      "8",
+      "-hide_banner",
+      // Redirect/Enable progress messages
+      "-progress",
+      "pipe:3",
+      // Set inputs
+      "-i",
+      "pipe:4",
+      "-i",
+      "pipe:5",
+      // Map audio & video from streams
+      "-map",
+      "0:a",
+      "-map",
+      "1:v",
+      // Keep encoding
+      "-c:v",
+      "copy",
+      // Define output file
+      "./videos/YouTube/out.mp4",
+    ],
+    {
+      windowsHide: true,
+      stdio: [
+        /* Standard: stdin, stdout, stderr */
+        "inherit",
+        "inherit",
+        "inherit",
+        /* Custom: pipe:3, pipe:4, pipe:5 */
+        "pipe",
+        "pipe",
+        "pipe",
+      ],
+    }
+  );
+  ffmpegProcess.on("close", () => {
+    console.log("done");
+    // Cleanup
+    process.stdout.write("\n\n\n\n");
+    // clearInterval(progressbarHandle);
+  });
+
+  // Link streams
+  // FFmpeg creates the transformer streams and we just have to insert / read data
+  ffmpegProcess.stdio[3].on("data", (chunk) => {
+    // Start the progress bar
+    // if (!progressbarHandle)
+    //   progressbarHandle = setInterval(showProgress, progressbarInterval);
+    // Parse the param=value list returned by ffmpeg
+    const lines = chunk.toString().trim().split("\n");
+    const args = {};
+    for (const l of lines) {
+      const [key, value] = l.split("=");
+      args[key.trim()] = value.trim();
+    }
+    tracker.merged = args;
+  });
+  audio.pipe(ffmpegProcess.stdio[4]);
+  video.pipe(ffmpegProcess.stdio[5]);
 });
 
 // Functions
