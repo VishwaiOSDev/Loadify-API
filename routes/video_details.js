362af9ae2b1bb817e3db7d81b49e7c38c81bf651
MageshBabu K <mage1k99@fossfreaks.com>
2022-05-05 22:37:46 +0530

    use the controller

--- routes/video_details.js
+++ routes/video_details.js
@@ -1,24 +1,6 @@
 const router = require("express").Router();
+const { getDetails } = require("../controllers/youtube/youtube_controller");
 
-const constants = require("../lib/constants");
-const getVideoDetailsOf = require("../lib/get_video_details");
-const extractDetailsFrom = require("../lib/extract_details");
-const ytdl = require("ytdl-core");
-
-router.get("/", async (req, res) => {
-  const video_url = req.query.url;
-  try {
-    const video_details = await getVideoDetailsOf(video_url);
-    const details = extractDetailsFrom(
-      video_details,
-      constants.YOUTUBE_DETAILS
-    );
-    // Check the usage if the details needed YouTube URL
-    res.json(details);
-  } catch (err) {
-    console.log(`Error getting info ${err}`);
-    res.json({ message: "Something went wrong" + err });
-  }
-});
+router.get("/", getDetails);
 
 module.exports = router;
